81de441397343f7e12c26df8f848a749a1196dde
Kwetii <kwetschas@gmail.com>
2021-11-18 10:12:32 +0100

    Foresqaure research

--- Read_data.py
+++ Read_data.py
@@ -5,6 +5,3 @@ data = pd.read_csv('Charging_PA.csv',sep=',')
 print(data.head(10))
 print(data.info())
 print(data['Port Type'].value_counts())
-# Verteilung der öffentlich zugänglichen Ladepunkte auf die Bundesländer
-
-#print(data.where(data['Postleitzahl'] >= 78460, inplace = True))
